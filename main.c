383a1c61c7dcaa3f2b84c1e2049bae85bf94bb8e
switchcodeur <switchcodeur@switchcodeur.com>
2026-01-18 14:38:14 +0100

    Split main loop into multiple functions

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include "window.h"
 #include <GL/gl.h>
 
+int step(void);
+void draw(void);
+
 static const char *window_name = "Hello world!";
 static const int width = 800;
 static const int height = 600;
@@ -10,12 +13,27 @@ main()
 {
 	window_create(window_name, 1, width, height);
 	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-	while (window_update()) {
-		if (input_key_down(INPUT_ESCAPE)) {
-			window_destroy();
-		}
-		glClear(GL_COLOR_BUFFER_BIT);
+	while (step()) {
+		draw();
 	}
 	window_destroy();
 	return 0;
+}
+
+int
+step()
+{
+	if (!window_update()) {
+		return 0;
+	}
+	if (input_key_down(INPUT_ESCAPE)) {
+		return 0;
+	}
+	return 1;
+}
+
+void
+draw(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
 }
